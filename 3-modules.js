ecf94c6962aae702f6e18c15be0ee503dccdfb54
shreya <bansalshreya200204@gmail.com>
2024-02-19 23:33:13 +0530

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,16 @@
+// const john = 'john'
+// const peter ='peter'
+
+// const sayHi = (name) => {
+//     console.log(`Hello there ${name}`)
+// }
+const names = require('./4-names');
+const sayHi = require('./5-utils')
+const data = require('./6-alternative_flavour');
+//const sum = require('./7-mind_grenade');
+// console.log(names);
+// console.log(data)
+require('./7-mind_grenade')
+// sayHi('susan')
+// sayHi(names.john)
+// sayHi(names.peter)
